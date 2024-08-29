3bb5e43a2285675ac4f0b49a73e7ffe069c5ba65
Vinay-Sriram-Tummidi <vinaysriram.tummidi@gmail.com>
2024-08-29 21:26:51 +0530

    changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./Redux/Store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.querySelector("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
